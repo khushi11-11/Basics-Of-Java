53c2ed8271325c062ce224a5cb48dc331b05d281
Khushi Verma <khushi7991012@gmail.com>
2025-01-10 18:53:46 +0530

    Basics of JAVA

--- src/com/basic/prg1/TypeCasting.java
+++ src/com/basic/prg1/TypeCasting.java
@@ -12,16 +12,44 @@ package com.basic.prg1;
  explicit conversion
  int num = (int)(67.5f);
  compressing a larger no. to a smaller type, explicitly
-*/
 
+
+ AUTOMATIC TYPE PROMOTION IN EXPRESSIONS :-
+ BYTE type has a range of 256, when we write something like byte num = 257, it will give 1 as output
+ it basically divides the number with the highest no. in range and gives the remainder as output
+*/
 
 import java.util.Scanner;
 
 public class TypeCasting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number ");
-        float num = input.nextFloat();
-        System.out.println(num);
+//        System.out.print("Enter a number ");
+//        float num = input.nextFloat();
+//        System.out.println(num);
+
+        byte b = 40;
+        byte a = 50;
+        byte c = 100;
+        int d = (a*b)/c;
+//        System.out.println(d);
+// here a*b = 2000 and /c = 20
+// why and how is a*b is storing the int type of the result and not the byte type of it
+// JAVA automatically promotes the number with lower type to the required type, here int, when evaluating expressions
+
+        byte x = 50;
+//        x = x*2;
+//        this will give an error, as while evaluating expressions the variables change their type to int
+//        which means b*2 is now of type int and now it can no more be stored in the variable x of type byte
+//        x is of byte type, but x*2 whole (as expression) is of int type
+
+        int number = 'A';
+//        System.out.println(number);
+//        gives 65 in output, the ASCII value of 'A', automatic type conversion
+
+        System.out.println("नमस्ते");
+        System.out.println("你好");
+//        JAVA is a unicode programming language, can print any language
     }
 }
+
